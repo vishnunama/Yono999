65a1aa11d985282a7f82e40f8e81aedc5d956e85
Vishnu nama <vishunama9829@gmail.com>
2025-02-23 11:19:12 +0530

    fina

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,17 +6,25 @@ import DownloadButton from "./DownloadButton";
 
 function Home() {
   return (
-    <div className="home-container">
-        <div style={{background:"#0D6F69", width:"100%", borderTop:"4px solid #d4af37", marginTop:"500px"}}>
-      <div className="top-title">Top Games On Yono 999</div>
-      <p className="sub-title">
-        Play over <span className="highlight">60+</span> Games and Win Cash
-      </p>
+    <div
+      className="home-container"
+      style={{
+        backgroundImage: `url("H51.png")`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div style={{ background: "#0D6F69", width: "100%", borderTop: "4px solid #d4af37", marginTop: "500px" }}>
+        <div className="top-title">Top Games On Yono 999</div>
+        <p className="sub-title">
+          Play over <span className="highlight">60+</span> Games and Win Cash
+        </p>
       </div>
       <div>
-        <Game/>
-        <FeatureSection/>
-        <DownloadButton/>
+        <Game />
+        <FeatureSection />
+        <DownloadButton />
       </div>
     </div>
   );
